f77c73c09527697815eca6942d9b9319bf1b1d41
aavila01sufro <a.avila01@ufromail.cl>
2020-05-07 01:37:56 -0400

    Estado inicial con método de validación y menú.

--- src/calc/CalculadoraConMetodos.java
+++ src/calc/CalculadoraConMetodos.java
@@ -7,6 +7,7 @@ public class CalculadoraConMetodos {
 
     public static void main(String[] args){
         int opcion;
+
         do {
             opcion = menu();
             selecOperacion(opcion);
